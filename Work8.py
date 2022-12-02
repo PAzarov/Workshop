e586b29acb9f9cfae88f7bc0e51070463aed775e
PAzarov <116663288+PAzarov@users.noreply.github.com>
2022-12-02 15:30:43 +0300

    Update Work8.py

--- Work8.py
+++ Work8.py
@@ -15,11 +15,16 @@ for i in range(N):
 print('Сумма:', s)
 print('Число:', pol)
 #В1.2
-N = int(input())
-M = int(input())
-B = [[random.randrange(10) for i in range(N)] for j in range(N)]
-for i in range(N, M):
-    if B[i][j] >= 0:
-        print('Максимальный элемент:', B)
-    if B[i][j] < 0:
-        print('Минимальный элемент:', B)
+N = int(input('N: '))
+M = int(input('M: '))
+B = [[random.randrange(10) for i in range(M)] for j in range(N)] 
+for i, row in enumerate(B):
+    max = min = 0
+    for j, elem in enumerate(row):
+        if elem > row[max]:
+            max = j
+        if elem < row[min]:
+            min = j
+    row[max], row[0] = row[0], row[max]
+    row[min], row[-1] = row[-1], row[min]
+print(B)
